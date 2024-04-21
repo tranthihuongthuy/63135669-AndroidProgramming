44255c461e44912cb5beb28bc3e4df0a007ca343
thirr <tranthihuongthuy27@gmail.com>
2024-04-21 10:28:29 +0700

    Ví dụ 2 Đăng Nhập Start

---
+++ Vidu2_Dangnhap/app/src/main/java/till/huongthuy/vidu2_dangnhap/MainActivity.java
@@ -0,0 +1,24 @@
+package till.huongthuy.vidu2_dangnhap;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void DangNhap(View v){
+        Intent iLogin = new Intent(this, ActivityLogin.class);
+        startActivity(iLogin);
+    }
+}
