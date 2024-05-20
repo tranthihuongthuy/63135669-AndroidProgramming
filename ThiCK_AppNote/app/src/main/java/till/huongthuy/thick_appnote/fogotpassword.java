abb8c30c76f1eb9c103ac62e6fc04d3c18038a4e
thirr <tranthihuongthuy27@gmail.com>
2024-05-20 15:22:03 +0700

    ThiCK App Note Start

---
+++ ThiCK_AppNote/app/src/main/java/till/huongthuy/thick_appnote/fogotpassword.java
@@ -0,0 +1,18 @@
+package till.huongthuy.thick_appnote;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class fogotpassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fogotpassword);
+    }
+}
